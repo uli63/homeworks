dfd7a446056650135f6471355503f58635207fe0
uli63 <saitysamara@yandex.ru>
2021-06-20 17:09:38 +0400

    missoin failed)))

--- task.js
+++ task.js
@@ -1,7 +1,25 @@
-'use strict';
+'use strict'
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  let D = (b * b) - (4 * a * c);
+
+  if (D > 0) {
+    let x1 = (-b + Math.sqrt(d) )/(2a);
+    let x2 = (-b - Math.sqrt(d) )/(2a);
+    
+    arr.push(x1, x2);
+
+  } else if (D === 0) {
+    let x = -b/(2*a);
+
+    arr.push(x);
+  
+  } else {
+
+    arr = [];
+
+  
   return arr; // array
 }
 
